5e9b2aec83579bce6a8432141976641977ccea8b
Sadaf Rahman <62661608+rahmansadaf46@users.noreply.github.com>
2022-09-15 02:59:50 +0600

    homepage update

--- src/components/Home/Navbar/Navbar.js
+++ src/components/Home/Navbar/Navbar.js
@@ -44,7 +44,7 @@ const Navbar = () => {
                     </div>
                     <div className="col-md-10 mt-3 pl-5">
                         <h4 style={{ color: '#FB9937' }} className="">Institute of Science and Technology</h4>
-                        <small style={{ color: '#7AB259' }} className="">Since 1993</small>
+                        <small style={{ color: '#7AB259', fontSize:'14px' }} className="">Since 1993</small>
                     </div>
                 </div>
 
